85ba85b26a1cc8816a65e742029dcd7d15e15d0e
adofsauron <adofsauron@163.com>
2023-02-22 14:08:35 +0800

    fix cascades dev

--- cascades/src/stdafx.h
+++ cascades/src/stdafx.h
@@ -8,16 +8,16 @@
 
 #if _MSC_VER >= 1000
 #pragma once
-#endif // _MSC_VER >= 1000
+#endif  // _MSC_VER >= 1000
 
-#define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
+#define VC_EXTRALEAN  // Exclude rarely-used stuff from Windows headers
 
-#include <afxwin.h>         // MFC core and standard components
-#include <afxext.h>         // MFC extensions
+#include <afxwin.h>  // MFC core and standard components
+#include <afxext.h>  // MFC extensions
 #include <afxtempl.h>
 #ifndef _AFX_NO_AFXCMN_SUPPORT
-#include <afxcmn.h>			// MFC support for Windows Common Controls
-#endif // _AFX_NO_AFXCMN_SUPPORT
+#include <afxcmn.h>  // MFC support for Windows Common Controls
+#endif               // _AFX_NO_AFXCMN_SUPPORT
 
 #include <assert.h>
 #include <math.h>
@@ -35,11 +35,11 @@ using namespace std;
 #include "wcol.h"
 #include "defs.h"
 #ifdef USE_MEMORY_MANAGER
-	#include "bm.h"
+#include "bm.h"
 #endif
 #include "supp.h"
 
 //{{AFX_INSERT_LOCATION}}
 // Microsoft Developer Studio will insert additional declarations immediately before the previous line.
 
-#endif // !defined(AFX_STDAFX_H__1801B689_FC90_11D0_8172_ED1BA299B528__INCLUDED_)
+#endif  // !defined(AFX_STDAFX_H__1801B689_FC90_11D0_8172_ED1BA299B528__INCLUDED_)
